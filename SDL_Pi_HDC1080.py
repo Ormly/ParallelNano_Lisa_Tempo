760da10d1c188b1f7839ccc1bab09f9d8f1c77f8
Mohab Osman <mohabmosman94@hotmail.com>
2020-10-15 23:21:36 +0200

    Refactor library

--- SDL_Pi_HDC1080.py
+++ SDL_Pi_HDC1080.py
@@ -18,7 +18,7 @@ HDC1080_TEMPERATURE_REGISTER =          (0x00)
 HDC1080_HUMIDITY_REGISTER =             (0x01)
 HDC1080_CONFIGURATION_REGISTER =        (0x02)
 HDC1080_MANUFACTURERID_REGISTER =       (0xFE)
-HDC1080_DEVICEID_REGISTER =         (0xFF)
+HDC1080_DEVICEID_REGISTER =             (0xFF)
 HDC1080_SERIALIDHIGH_REGISTER =         (0xFB)
 HDC1080_SERIALIDMID_REGISTER =          (0xFC)
 HDC1080_SERIALIDBOTTOM_REGISTER =       (0xFD)
@@ -27,34 +27,34 @@ HDC1080_SERIALIDBOTTOM_REGISTER =       (0xFD)
 
 #Configuration Register Bits
 
-HDC1080_CONFIG_RESET_BIT =              (0x8000)
-HDC1080_CONFIG_HEATER_ENABLE =          (0x2000)
-HDC1080_CONFIG_ACQUISITION_MODE =       (0x1000)
-HDC1080_CONFIG_BATTERY_STATUS =         (0x0800)
-HDC1080_CONFIG_TEMPERATURE_RESOLUTION = (0x0400)
-HDC1080_CONFIG_HUMIDITY_RESOLUTION_HBIT =    (0x0200)
-HDC1080_CONFIG_HUMIDITY_RESOLUTION_LBIT =    (0x0100)
+HDC1080_CONFIG_RESET_BIT =                    (0x8000)
+HDC1080_CONFIG_HEATER_ENABLE =                (0x2000)
+HDC1080_CONFIG_ACQUISITION_MODE =             (0x1000)
+HDC1080_CONFIG_BATTERY_STATUS =               (0x0800)
+HDC1080_CONFIG_TEMPERATURE_RESOLUTION =       (0x0400)
+HDC1080_CONFIG_HUMIDITY_RESOLUTION_HBIT =     (0x0200)
+HDC1080_CONFIG_HUMIDITY_RESOLUTION_LBIT =     (0x0100)
 
 HDC1080_CONFIG_TEMPERATURE_RESOLUTION_14BIT = (0x0000)
 HDC1080_CONFIG_TEMPERATURE_RESOLUTION_11BIT = (0x0400)
 
-HDC1080_CONFIG_HUMIDITY_RESOLUTION_14BIT = (0x0000)
-HDC1080_CONFIG_HUMIDITY_RESOLUTION_11BIT = (0x0100)
-HDC1080_CONFIG_HUMIDITY_RESOLUTION_8BIT = (0x0200)
+HDC1080_CONFIG_HUMIDITY_RESOLUTION_14BIT =    (0x0000)
+HDC1080_CONFIG_HUMIDITY_RESOLUTION_11BIT =    (0x0100)
+HDC1080_CONFIG_HUMIDITY_RESOLUTION_8BIT =     (0x0200)
 
-I2C_SLAVE=0x0703
+I2C_SLAVE = 0x0703
 
 import struct, array, time, io, fcntl
 
-HDC1080_fw= 0
-HDC1080_fr= 0
+HDC1080_fw = 0
+HDC1080_fr = 0
 
 class SDL_Pi_HDC1080:
         def __init__(self, twi=1, addr=HDC1080_ADDRESS ):
                 global HDC1080_fr, HDC1080_fw
                 
-                HDC1080_fr= io.open("/dev/i2c-"+str(twi), "rb", buffering=0)
-                HDC1080_fw= io.open("/dev/i2c-"+str(twi), "wb", buffering=0)
+                HDC1080_fr = io.open("/dev/i2c-" + str(twi), "rb", buffering=0)
+                HDC1080_fw = io.open("/dev/i2c-" + str(twi), "wb", buffering=0)
 
                 # set device address
                 fcntl.ioctl(HDC1080_fr, I2C_SLAVE, HDC1080_ADDRESS)
@@ -63,9 +63,9 @@ class SDL_Pi_HDC1080:
 
                 config = HDC1080_CONFIG_ACQUISITION_MODE 
 
-                s = [HDC1080_CONFIGURATION_REGISTER,config>>8,0x00]
-                s2 = bytearray( s )
-                HDC1080_fw.write( s2 ) #sending config register bytes
+                s = [HDC1080_CONFIGURATION_REGISTER, config >> 8, config & 0xFF]
+                s2 = bytearray(s)
+                HDC1080_fw.write(s2) #sending config register bytes
                 time.sleep(0.015)               # From the data sheet
                 
                 #       0x10(48)    Temperature, Humidity enabled, Resolultion = 14-bits, Heater off
@@ -79,13 +79,12 @@ class SDL_Pi_HDC1080:
                
 
                 s = [HDC1080_TEMPERATURE_REGISTER] # temp
-                s2 = bytearray( s )
-                HDC1080_fw.write( s2 )
+                s2 = bytearray(s)
+                HDC1080_fw.write(s2)
                 time.sleep(0.0625)              # From the data sheet
 
                 data = HDC1080_fr.read(2) #read 2 byte temperature data
                 buf = array.array('B', data)
-                #print ( "Temp: %f 0x%X %X" % (  ((((buf[0]<<8) + (buf[1]))/65536.0)*165.0 ) - 40.0   ,buf[0],buf[1] )  )
 
                 
                 # Convert the data
@@ -99,13 +98,12 @@ class SDL_Pi_HDC1080:
                 time.sleep(0.015)               # From the data sheet
 
                 s = [HDC1080_HUMIDITY_REGISTER] # hum
-                s2 = bytearray( s )
-                HDC1080_fw.write( s2 )
+                s2 = bytearray(s)
+                HDC1080_fw.write(s2)
                 time.sleep(0.0625)              # From the data sheet
 
                 data = HDC1080_fr.read(2) #read 2 byte humidity data
                 buf = array.array('B', data)
-                #print ( "Humidity: %f 0x%X %X " % (  ((((buf[0]<<8) + (buf[1]))/65536.0)*100.0 ),  buf[0], buf[1] ) )
                 humidity = (buf[0] * 256) + buf[1]
                 humidity = (humidity / 65536.0) * 100.0
                 return humidity
@@ -115,16 +113,14 @@ class SDL_Pi_HDC1080:
 
 
                 s = [HDC1080_CONFIGURATION_REGISTER] # temp
-                s2 = bytearray( s )
-                HDC1080_fw.write( s2 )
+                s2 = bytearray(s)
+                HDC1080_fw.write(s2)
                 time.sleep(0.0625)              # From the data sheet
 
                 data = HDC1080_fr.read(2) #read 2 byte config data
 
                 buf = array.array('B', data)
 
-
-                #print "register=%X %X"% (buf[0], buf[1])
                 return buf[0]*256+buf[1]
 
        
@@ -132,9 +128,9 @@ class SDL_Pi_HDC1080:
                 # Read config register
                 config = self.readConfigRegister()
                 config = config | HDC1080_CONFIG_HEATER_ENABLE 
-                s = [HDC1080_CONFIGURATION_REGISTER,config>>8,0x00]
-                s2 = bytearray( s )
-                HDC1080_fw.write( s2 ) #sending config register bytes
+                s = [HDC1080_CONFIGURATION_REGISTER, config >> 8 , config & 0xFF]
+                s2 = bytearray(s)
+                HDC1080_fw.write(s2) #sending config register bytes
                 time.sleep(0.015)               # From the data sheet
 
                 return
@@ -143,9 +139,9 @@ class SDL_Pi_HDC1080:
                 # Read config register
                 config = self.readConfigRegister()
                 config = config & ~HDC1080_CONFIG_HEATER_ENABLE 
-                s = [HDC1080_CONFIGURATION_REGISTER,config>>8,0x00]
-                s2 = bytearray( s )
-                HDC1080_fw.write( s2 ) #sending config register bytes
+                s = [HDC1080_CONFIGURATION_REGISTER, config >> 8, config & 0xFF]
+                s2 = bytearray(s)
+                HDC1080_fw.write(s2) #sending config register bytes
                 time.sleep(0.015)               # From the data sheet
 
                 return
@@ -156,9 +152,9 @@ class SDL_Pi_HDC1080:
                 # Read config register
                 config = self.readConfigRegister()
                 config = (config & ~0x0300) | resolution 
-                s = [HDC1080_CONFIGURATION_REGISTER,config>>8,0x00]
-                s2 = bytearray( s )
-                HDC1080_fw.write( s2 ) #sending config register bytes
+                s = [HDC1080_CONFIGURATION_REGISTER, config >> 8, config & 0xFF]
+                s2 = bytearray(s)
+                HDC1080_fw.write(s2) #sending config register bytes
                 time.sleep(0.015)               # From the data sheet
                 return
 
@@ -167,9 +163,9 @@ class SDL_Pi_HDC1080:
                 config = self.readConfigRegister()
                 config = (config & ~0x0400) | resolution 
                
-                s = [HDC1080_CONFIGURATION_REGISTER,config>>8,0x00]
-                s2 = bytearray( s )
-                HDC1080_fw.write( s2 ) #sending config register bytes
+                s = [HDC1080_CONFIGURATION_REGISTER, config >> 8, config & 0xFF]
+                s2 = bytearray(s)
+                HDC1080_fw.write(s2) #sending config register bytes
                 time.sleep(0.015)               # From the data sheet
                 
                 
@@ -192,8 +188,8 @@ class SDL_Pi_HDC1080:
         def readManufacturerID(self):
 
             s = [HDC1080_MANUFACTURERID_REGISTER] # temp
-            s2 = bytearray( s )
-            HDC1080_fw.write( s2 )
+            s2 = bytearray(s)
+            HDC1080_fw.write(s2)
             time.sleep(0.0625)              # From the data sheet
     
             data = HDC1080_fr.read(2) #read 2 byte config data
@@ -204,8 +200,8 @@ class SDL_Pi_HDC1080:
         def readDeviceID(self):
     
             s = [HDC1080_DEVICEID_REGISTER] # temp
-            s2 = bytearray( s )
-            HDC1080_fw.write( s2 )
+            s2 = bytearray(s)
+            HDC1080_fw.write(s2)
             time.sleep(0.0625)              # From the data sheet
     
             data = HDC1080_fr.read(2) #read 2 byte config data
@@ -218,24 +214,24 @@ class SDL_Pi_HDC1080:
             serialNumber = 0
     
             s = [HDC1080_SERIALIDHIGH_REGISTER] # temp
-            s2 = bytearray( s )
-            HDC1080_fw.write( s2 )
+            s2 = bytearray(s)
+            HDC1080_fw.write(s2)
             time.sleep(0.0625)              # From the data sheet
             data = HDC1080_fr.read(2) #read 2 byte config data
             buf = array.array('B', data)
             serialNumber = buf[0]*256+ buf[1] 
     
             s = [HDC1080_SERIALIDMID_REGISTER] # temp
-            s2 = bytearray( s )
-            HDC1080_fw.write( s2 )
+            s2 = bytearray(s)
+            HDC1080_fw.write(s2)
             time.sleep(0.0625)              # From the data sheet
             data = HDC1080_fr.read(2) #read 2 byte config data
             buf = array.array('B', data)
             serialNumber = serialNumber*256 + buf[0]*256 + buf[1] 
     
             s = [HDC1080_SERIALIDBOTTOM_REGISTER] # temp
-            s2 = bytearray( s )
-            HDC1080_fw.write( s2 )
+            s2 = bytearray(s)
+            HDC1080_fw.write(s2)
             time.sleep(0.0625)              # From the data sheet
             data = HDC1080_fr.read(2) #read 2 byte config data
             buf = array.array('B', data)
