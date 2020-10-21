96c6557f1d3343510ba336d474333ca2545660d7
MohabOsman <mohabmosman94@hotmail.com>
2020-10-21 15:40:08 +0200

    Remove unused files

--- test_hdc1080.py
+++ test_hdc1080.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Test SDL_Pi_HDC1080
+# Test sdl_pi_hdc1080
 #
 # June 2017
 #
@@ -10,20 +10,20 @@
 import sys          
 import time
 import datetime
-import SDL_Pi_HDC1080
+import sdl_pi_hdc1080
 
 
 
 # Main Program
 print
 print ("")
-print ("Test SDL_Pi_HDC1080 Version 1.1 - SwitchDoc Labs")
+print ("Test sdl_pi_hdc1080 Version 1.1 - SwitchDoc Labs")
 print ("")
 print ("Sample uses 0x40 and SwitchDoc HDC1080 Breakout board ")
 print ("Program Started at:"+ time.strftime("%Y-%m-%d %H:%M:%S"))
 print ("")
 
-hdc1080 = SDL_Pi_HDC1080.SDL_Pi_HDC1080()
+hdc1080 = sdl_pi_hdc1080.SDL_Pi_HDC1080()
 
 print ("------------")
 print ("Manfacturer ID=0x%X"% hdc1080.readManufacturerID())  
@@ -44,23 +44,23 @@ print ("configure register = 0x%X" % hdc1080.readConfigRegister())
 
 # change temperature resolution
 print ("change temperature resolution")
-hdc1080.setTemperatureResolution(SDL_Pi_HDC1080.HDC1080_CONFIG_TEMPERATURE_RESOLUTION_11BIT)
+hdc1080.setTemperatureResolution(sdl_pi_hdc1080.HDC1080_CONFIG_TEMPERATURE_RESOLUTION_11BIT)
 # read configuration register
 print ("configure register = 0x%X" % hdc1080.readConfigRegister())
 # change temperature resolution
 print ("change temperature resolution")
-hdc1080.setTemperatureResolution(SDL_Pi_HDC1080.HDC1080_CONFIG_TEMPERATURE_RESOLUTION_14BIT)
+hdc1080.setTemperatureResolution(sdl_pi_hdc1080.HDC1080_CONFIG_TEMPERATURE_RESOLUTION_14BIT)
 # read configuration register
 print ("configure register = 0x%X" % hdc1080.readConfigRegister())
 
 # change humdity resolution
 print ("change humidity resolution")
-hdc1080.setHumidityResolution(SDL_Pi_HDC1080.HDC1080_CONFIG_HUMIDITY_RESOLUTION_8BIT)
+hdc1080.setHumidityResolution(sdl_pi_hdc1080.HDC1080_CONFIG_HUMIDITY_RESOLUTION_8BIT)
 # read configuration register
 print ("configure register = 0x%X" % hdc1080.readConfigRegister())
 # change humdity resolution
 print ("change humidity resolution")
-hdc1080.setHumidityResolution(SDL_Pi_HDC1080.HDC1080_CONFIG_HUMIDITY_RESOLUTION_14BIT)
+hdc1080.setHumidityResolution(sdl_pi_hdc1080.HDC1080_CONFIG_HUMIDITY_RESOLUTION_14BIT)
 # read configuration register
 print ("configure register = 0x%X" % hdc1080.readConfigRegister())
 
